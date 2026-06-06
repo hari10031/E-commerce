29a74987e7aee0235f4b24161ad127a6c3cadbae
hari10031 <245123751005@mvsrec.edu.in>
2026-06-06 22:40:15 +0530

    removed dist files

--- backend/dist/middleware/errorHandler.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare function errorHandler(err: Error, req: Request, res: Response, _next: NextFunction): void;
-export declare function notFound(req: Request, res: Response): void;
-//# sourceMappingURL=errorHandler.d.ts.map
--- backend/dist/services/storageService.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function uploadImage(buffer: Buffer, originalName: string, bucket: 'product-images' | 'category-images'): Promise<string>;
-export declare function deleteImage(bucket: string, filename: string): Promise<void>;
-//# sourceMappingURL=storageService.d.ts.map
